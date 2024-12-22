efd10f65016ec192935190343c261d72e2ee420e
infokey <infokey.hk@gmail.com>
2024-12-21 21:33:31 -0500

    fix cctv4

--- cmd/cctv4.bat
+++ cmd/cctv4.bat
@@ -1,2 +1,2 @@
 powershell -command "Get-Process chrome | ForEach-Object { $_.CloseMainWindow() | Out-Null}"
-start https://www.youtube.com/watch?v=twhG7_jvA8g
+start https://www.youtube.com/watch?v=SdzewdkJa-o
